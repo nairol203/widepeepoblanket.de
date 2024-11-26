460bc3c89a7c1fe4810fa666e60fdfc578b26c5a
nairol203 <florian@nairol.me>
2024-11-26 17:38:27 +0100

    added html + dockerfile

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+tailwind.config = {
+    theme: {
+        extend: {
+            colors: {
+	        primary: '#C21010',
+		secondary: '#E64848',
+		tertiary: '#fff',
+		background: '#fff',
+		text: '#000',
+	        card: '#f2f2f2',
+		hover: '#e9e9e9',
+		dark: {
+                    tertiary: '#545556',
+		    background: '#0f0f0f',
+		    text: '#f1f1f1',
+		    card: '#272727',
+		    hover: '#646464',
+		}
+	    }
+	}
+    }
+}
